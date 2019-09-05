5cf1953482559e464338225e8cca07b6c5510ea6
Ian Sawicki <iswick@gmail.com>
2019-09-05 13:45:10 -0700

    Update match_key_terms.py

--- match_key_terms.py
+++ match_key_terms.py
@@ -3,10 +3,10 @@ Set the campaign_cats value by entering a list of apparel categories between the
 Each category should be wrapped in quotes and separated from the next one by a comma. 
 """ 
 
-campaign_cats = ["Clutch Bags","Wallets", "Accessories"]
+campaign_cats = ["Jewellery", "Watches"]
 
 
-###Functional code starts here. 
+###Functional code begins here - edit at your own peril!
 
 campaign_cats = [i.upper() for i in campaign_cats]
 
@@ -14,22 +14,28 @@ def any_matches(a,b):
     if len([i for i in a if i in b]) >= 1:
         return(True)
     return(False)
-    
+
 def handle_user_events(event, lead, traveler, connected_record, resources):
     traveler.VarB = str(event.categories_list)
     traveler.VarG = str(event.subcategories_list)
     
     
 def handle_lead(event, lead, traveler, connected_record, resources):
-    myCats = connected_record.get_entities('Google BigQuery', 'UserEvents')[0].categories_list
-    mySubCats = connected_record.get_entities('Google BigQuery', 'UserEvents')[0].subcategories_list
-    allCats = myCats+", "+mySubCats
-    allCats = allCats.upper()
-    
-    if allCats is None:
+    user_events = connected_record.get_entities('Google BigQuery', 'UserEvents')
+    if user_events == None or len(user_events) == 0:
+        return     
+    myCats = user_events[0].categories_list
+    mySubCats = user_events[0].subcategories_list
+    allCats = ",".join([x for x in [myCats,mySubCats] if x != None or isinstance(x,str)])
+    if allCats is None or len(allCats)==0:
         return
+      
+    allCats = allCats.upper()
     traveler.VarB = any_matches(campaign_cats,allCats)
-        
+    
+    
+    
+###Event handlers        
 handler1 = NamedHandler(handle_user_events, 'handle_user_events')
 trigger1 = OnChangeToFieldTrigger('Google BigQuery', 'UserEvents', 'Id')
 advanced_rule_registry.register(trigger1, handler1)
